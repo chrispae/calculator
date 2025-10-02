5e74558330f6994f60bbb9078526c262a6582f43
Christof Paessler <christof.paessler@gmail.com>
2025-10-02 09:13:30 +0200

    added tests for basic calculator

--- tests/test_calculator.cpp
+++ tests/test_calculator.cpp
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#include <calculator/calculator.hpp>
 
 TEST(HelloTest, BasicAssertions)
 {
@@ -6,4 +7,30 @@ TEST(HelloTest, BasicAssertions)
     EXPECT_STREQ("hello", "hello");
     // Expect equality.
     EXPECT_EQ(7 * 6, 42);
+}
+
+TEST(SimpleOperators, TwoNumbers)
+{
+    Calculator calc{};
+    auto res1 = calc.solve(" 2 + 6");
+    EXPECT_EQ(res1, 8);
+    auto res2 = calc.solve(" 5 - 8");
+    EXPECT_EQ(res2, -3);
+    auto res3 = calc.solve(" 6 * 7");
+    EXPECT_EQ(res3, 42);
+    auto res4 = calc.solve(" 63 / 9");
+    EXPECT_EQ(res4, 7);
+}
+
+TEST(SimpleOperators, MoreNumbers)
+{
+    Calculator calc{};
+    auto res1 = calc.solve(" 2 + 6 * 3 - 7");
+    EXPECT_EQ(res1, 13);
+    auto res2 = calc.solve(" 5  * 8+ 4 - 3 - 10");
+    EXPECT_EQ(res2, 31);
+    auto res3 = calc.solve(" 6 / 2 + 8 * 3");
+    EXPECT_EQ(res3, 27);
+    auto res4 = calc.solve(" 10 + 63 / 9 - 5");
+    EXPECT_EQ(res4, 12);
 }
